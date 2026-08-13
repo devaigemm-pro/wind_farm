dc862855d0d2ddc0821def5aa7607a4e1d5126cc
G3R4RD <Apple@MacBook-Pro.local>
2026-08-13 10:07:16 -0400

    fix: image adjustments button background transparent when inactive

--- src/components/organisms/AnnotateStep.tsx
+++ src/components/organisms/AnnotateStep.tsx
@@ -732,7 +732,7 @@ export function AnnotateStep({ inspectionId, inspection, campaignId: propCampaig
             <button style={{ ...actionBtnStyle, borderColor: showBladeOverlay ? C.primary : undefined, background: showBladeOverlay ? C.primaryLight : undefined }} title="Blade face view" onClick={() => setShowBladeOverlay(v => !v)}>
               <svg width="18" height="18" viewBox="0 0 24 24" fill={showBladeOverlay ? C.primary : '#555'}><path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z"/></svg>
             </button>
-            <button style={{ ...actionBtnStyle, borderColor: showImageAdjust ? C.primary : undefined, background: showImageAdjust ? C.primaryLight : undefined }} title="Image adjustments" onClick={() => setShowImageAdjust(v => !v)}>
+            <button style={{ ...actionBtnStyle, borderColor: showImageAdjust ? C.primary : undefined, background: showImageAdjust ? C.primaryLight : 'transparent' }} title="Image adjustments" onClick={() => setShowImageAdjust(v => !v)}>
               <svg width="18" height="18" viewBox="0 0 24 24" fill={showImageAdjust ? C.primary : '#555'}><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18V4c4.41 0 8 3.59 8 8s-3.59 8-8 8z"/></svg>
             </button>
             <button style={actionBtnStyle} title={t('annotate.downloadPhoto')} onClick={() => {
